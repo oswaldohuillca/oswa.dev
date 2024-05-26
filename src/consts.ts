bae6cacbafc60d8d8366f7ea86212bcf1c7959fb
oswaldo <oswaldohuillca@gmail.com>
2024-05-26 11:01:55 -0500

    added const page title

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Oswadev'
+export const SITE_TITLE = 'Oswaldo Huillca Huamani'
+export const PAGE_TITLE = 'Oswa dev'
 export const SITE_DESCRIPTION = '¡Hola! Soy Oswa, un desarrollador web con experiencia en la creación de sitios web dinámicos y atractivos. Desde diseños responsivos hasta funcionalidades personalizadas, estoy aquí para convertir tus ideas en realidad. Explora mi portfolio y descubre cómo puedo ayudarte a destacar en la web.'
